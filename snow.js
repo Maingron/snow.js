a39ac024d477d0242fba6eccc82d57ea7fc0dde0
Maingron <robin@maingron.com>
2020-07-26 17:28:38 +0200

    Add snow.config.autoFixScriptTag and add function initSnow()

--- snow.js
+++ snow.js
@@ -12,6 +12,7 @@ var snow = data["snow"];
 snow.config = {
     "snowContainer": document.body,
     "snowChars": ["*","*","*","+"],
+    "autoFixScriptTag": false, // Recommended: true. If true, make sure the snow.js file is called snow.js or snow.min.js. Might not have a big effect.
     "tickTime": 25, //ms
     "maxSnow": window.innerHeight / 8,
     "jitterAmount": 3,
@@ -21,6 +22,71 @@ snow.config = {
     "snowSizes": ["15px", "20px", "25px","30px","35px","40px","45px"],
     "snowColors": ["#fff","#fff","#fff","#fff","#fff","#fff","#99f","#f99","#edf","#afa"],
     "snowFont": "Calibri, Jokerman, Arial, Tahoma, sans-serif"
+}
+
+if(snow.config.autoFixScriptTag) {
+    snow.elements.scripts = document.getElementsByTagName("script");
+    for(var i = 0; snow.elements.scripts.length > i; i++) {
+        if(snow.elements.scripts[i] && snow.elements.scripts[i].getAttribute("src")) {
+            if(snow.elements.scripts[i].getAttribute("src").indexOf("snow.js") > -1 || snow.elements.scripts[i].getAttribute("src").indexOf("snow.minjs") > -1) {
+                snow.elements.script = snow.elements.snowScript = snow.elements.scripts[i];
+                break;
+            }
+        }
+
+    }
+    if(!snow.elements.script) {
+        console.warn("snow.js is not called snow.js or snow.min.js - snow.config.autoFixScriptTag was ignored and set to false.");
+        snow.config.autoFixScriptTag = false;
+    } else {
+        snow.elements.script.setAttribute("async",true);
+        snow.elements.script.setAttribute("charset","UTF-8");
+    }
+}
+
+function initSnow() {
+    addElement("style", document.body, "snowstyle");
+    document.getElementsByClassName("snowstyle")[0].innerHTML = `
+    .asnow {
+        position:fixed;
+        display:inline;
+        height:0;
+        width:0;
+        overflow:visible;
+        top:-50px;
+        font-family: `+snow.config.snowFont+`;
+        transition :`+snow.config.cssTransition+`s;
+        font-style: normal;
+        pointer-events: none;
+    }
+    `;
+
+    for (var i = 0; i < snow.config.maxSnow; i++) {
+        addElement("i", snow.config.snowContainer, "asnow");
+    }
+    snow.elements.snowflakes = document.getElementsByClassName("asnow");
+
+    for (var i = 0; i < snow.config.maxSnow; i++) {
+        snow.elements.snowflakes[i].left = (window.innerWidth / snow.config.maxSnow) * i; // set initial position left
+        snow.elements.snowflakes[i].top = -(Math.random() * snow.config.initialYDistance);
+    }
+
+    window.setInterval(function () {
+
+        for (var i = 0; i < snow.elements.snowflakes.length; i++) {
+            var j = snow.elements.snowflakes[i];
+
+            if (j.top < window.innerHeight) {
+                j.top = j.top + (Math.random() * snow.config.gravityAmount);
+                j.style.top = j.top + "px";
+            } else {
+                j.top = -60;
+            }
+
+            j.left = j.left + ((Math.random() * snow.config.jitterAmount) - (snow.config.jitterAmount / 2)); // add jittering
+            j.style.left = j.left + "px";
+        }
+    }, snow.config.tickTime);
 }
 
 function addElement(which, where, nclass, nid) {
@@ -43,48 +109,4 @@ function addElement(which, where, nclass, nid) {
 }
 
 
-
-addElement("style", document.body, "snowstyle");
-document.getElementsByClassName("snowstyle")[0].innerHTML = `
-.asnow {
-    position:fixed;
-    display:inline;
-    height:0;
-    width:0;
-    overflow:visible;
-    top:-50px;
-    font-family: `+snow.config.snowFont+`;
-    transition :`+snow.config.cssTransition+`s;
-    font-style: normal;
-    pointer-events: none;
-}
-`;
-
-
-for (var i = 0; i < snow.config.maxSnow; i++) {
-    addElement("i", snow.config.snowContainer, "asnow");
-}
-snow.elements.snowflakes = document.getElementsByClassName("asnow");
-
-for (var i = 0; i < snow.config.maxSnow; i++) {
-    snow.elements.snowflakes[i].left = (window.innerWidth / snow.config.maxSnow) * i; // set initial position left
-    snow.elements.snowflakes[i].top = -(Math.random() * snow.config.initialYDistance);
-}
-
-
-window.setInterval(function () {
-
-    for (var i = 0; i < snow.elements.snowflakes.length; i++) {
-        var j = snow.elements.snowflakes[i];
-
-        if (j.top < window.innerHeight) {
-            j.top = j.top + (Math.random() * snow.config.gravityAmount);
-            j.style.top = j.top + "px";
-        } else {
-            j.top = -60;
-        }
-
-        j.left = j.left + ((Math.random() * snow.config.jitterAmount) - (snow.config.jitterAmount / 2)); // add jittering
-        j.style.left = j.left + "px";
-    }
-}, snow.config.tickTime);
+initSnow();
